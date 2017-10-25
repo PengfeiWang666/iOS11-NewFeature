9e2b9ecf1b33f0d3f906721d9ea02854b45cf2a3
WangPengfei <wpf_silence@163.com>
2017-10-25 17:21:26 +0800

    添加资源文件

---
+++ NewFeature/NewFeature/UICollectionView-Drag&Drop/WPFImageCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  WPFImageCollectionViewCell.h
+//  NewFeature
+//
+//  Created by Leon on 2017/10/25.
+//  Copyright © 2017年 Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WPFImageCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ NewFeature/NewFeature/UICollectionView-Drag&Drop/WPFImageCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  WPFImageCollectionViewCell.m
+//  NewFeature
+//
+//  Created by Leon on 2017/10/25.
+//  Copyright © 2017年 Leon. All rights reserved.
+//
+
+#import "WPFImageCollectionViewCell.h"
+
+@implementation WPFImageCollectionViewCell
+
+@end
