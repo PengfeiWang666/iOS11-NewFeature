eecdc5fedba80d84b14b1dfa8162d876912f4f48
WangPengfei <wpf_silence@163.com>
2017-10-25 18:07:55 +0800

    完成 drop

--- NewFeature/NewFeature/UICollectionView-Drag&Drop/WPFDragCollectionViewController.m
+++ NewFeature/NewFeature/UICollectionView-Drag&Drop/WPFDragCollectionViewController.m
@@ -11,7 +11,7 @@
 
 static NSString *imageCellIdentifier = @"imageCellIdentifier";
 
-@interface WPFDragCollectionViewController () <UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDragDelegate>
+@interface WPFDragCollectionViewController () <UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDragDelegate, UICollectionViewDropDelegate>
 
 @property (nonatomic, strong) UICollectionViewFlowLayout *flowLayout;
 @property (nonatomic, strong) UICollectionView *collectionView;
@@ -52,6 +52,8 @@ static NSString *imageCellIdentifier = @"imageCellIdentifier";
     _collectionView.delegate = self;
     _collectionView.dataSource = self;
     _collectionView.dragDelegate = self;
+    _collectionView.dropDelegate = self;
+    // 该属性在 iPad 上默认是YES，在 iPhone 默认是 NO
     _collectionView.dragInteractionEnabled = YES;
     _collectionView.backgroundColor = [UIColor whiteColor];
 
@@ -94,11 +96,11 @@ static NSString *imageCellIdentifier = @"imageCellIdentifier";
  * If not implemented, or if an empty array is returned, no items will be added to the drag and the gesture
  * will be handled normally.
  */
-- (NSArray<UIDragItem *> *)collectionView:(UICollectionView *)collectionView itemsForAddingToDragSession:(id<UIDragSession>)session atIndexPath:(NSIndexPath *)indexPath point:(CGPoint)point {
-    NSItemProvider *itemProvider = [[NSItemProvider alloc] initWithObject:self.dataSource[indexPath.item]];
-    UIDragItem *item = [[UIDragItem alloc] initWithItemProvider:itemProvider];
-    return @[item];
-}
+//- (NSArray<UIDragItem *> *)collectionView:(UICollectionView *)collectionView itemsForAddingToDragSession:(id<UIDragSession>)session atIndexPath:(NSIndexPath *)indexPath point:(CGPoint)point {
+//    NSItemProvider *itemProvider = [[NSItemProvider alloc] initWithObject:self.dataSource[indexPath.item]];
+//    UIDragItem *item = [[UIDragItem alloc] initWithItemProvider:itemProvider];
+//    return @[item];
+//}
 
 /** 允许对从取消或返回到 CollectionView 的 item 使用自定义预览
     如果该方法没有实现或者返回nil，那么整个 cell 将用于预览
@@ -111,6 +113,63 @@ static NSString *imageCellIdentifier = @"imageCellIdentifier";
     CGRect rect = CGRectMake(0, 0, previewLength, previewLength);
     parameters.visiblePath = [UIBezierPath bezierPathWithRoundedRect:rect cornerRadius:5];
     return parameters;
+}
+
+#pragma mark - UICollectionViewDropDelegate
+
+
+/** 当用户开始进行 drop 操作的时候会调用这个方法
+   使用 dropCoordinator 去置顶如果处理当前 drop 会话的item 到指定的最终位置，同时也会根据drop item返回的数据更新数据源
+   如果该方法不做任何事，将会执行默认的动画
+ */
+- (void)collectionView:(UICollectionView *)collectionView performDropWithCoordinator:(id<UICollectionViewDropCoordinator>)coordinator {
+    
+    NSIndexPath *destinationIndexPath = coordinator.destinationIndexPath;
+    // 如果开始拖拽的 indexPath 和 要释放的目标 indexPath 一致，就不做处理
+    if (self.dragIndexPath.section == destinationIndexPath.section && self.dragIndexPath.row == destinationIndexPath.row) {
+        return;
+    }
+    
+    [collectionView performBatchUpdates:^{
+        // 目标 cell 换位置
+        [self.dataSource exchangeObjectAtIndex:destinationIndexPath.item withObjectAtIndex:self.dragIndexPath.item];
+        [collectionView moveItemAtIndexPath:self.dragIndexPath toIndexPath:destinationIndexPath];
+    } completion:^(BOOL finished) {
+        
+    }];
+}
+
+/* Called frequently while the drop session being tracked inside the collection view's coordinate space.
+ * When the drop is at the end of a section, the destination index path passed will be for a item that does not yet exist (equal
+ * to the number of items in that section), where an inserted item would append to the end of the section.
+ * The destination index path may be nil in some circumstances (e.g. when dragging over empty space where there are no cells).
+ * Note that in some cases your proposal may not be allowed and the system will enforce a different proposal.
+ * You may perform your own hit testing via -[UIDropSession locationInView]
+ */
+- (UICollectionViewDropProposal *)collectionView:(UICollectionView *)collectionView dropSessionDidUpdate:(id<UIDropSession>)session withDestinationIndexPath:(nullable NSIndexPath *)destinationIndexPath {
+    
+    /**
+      Collection view will accept the drop, but the location is not yet known and will be determined later.
+     * Will not open a gap. You may wish to provide some visual treatment to communicate this to the user.
+    UICollectionViewDropIntentUnspecified,
+    
+    The drop will be placed in item(s) inserted at the destination index path.
+     * Opens a gap at the specified location simulating the final dropped layout.
+    UICollectionViewDropIntentInsertAtDestinationIndexPath,
+    
+     The drop will be placed inside the item at the destination index path (e.g. the item is a container of other items).
+     * Will not open a gap. Collection view will highlight the item at the destination index path.
+    UICollectionViewDropIntentInsertIntoDestinationIndexPath,
+     */
+    UICollectionViewDropProposal *dropProposal;
+    // 如果是另外一个app，localDragSession为nil，此时就要执行copy，通过这个属性判断是否是在当前app中释放，当然只有 iPad 才需要这个适配
+    if (session.localDragSession) {
+        dropProposal = [[UICollectionViewDropProposal alloc] initWithDropOperation:UIDropOperationMove intent:UICollectionViewDropIntentInsertAtDestinationIndexPath];
+    } else {
+        dropProposal = [[UICollectionViewDropProposal alloc] initWithDropOperation:UIDropOperationCopy intent:UICollectionViewDropIntentInsertAtDestinationIndexPath];
+    }
+    
+    return dropProposal;
 }
 
 #pragma mark - Getters && Setters
